37035ad9fda28b77fe493928f7121f7cd95b0254
Sachin Singh Jagawat <sachinsinghjagawat@gmail.com>
2021-10-13 07:54:39 +0530

    All issues resolved till truffle configurations

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
-const Tether = artifacts.require('Migrations');
+// eslint-disable-next-line no-undef
+const Tether = artifacts.require('Tether');
 
 module.exports = async function (deployer){
     await deployer.deploy(Tether);
--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
 contract Tether {
-    string public name= "Tether";
-    string public symbol= "USDT";
-    uint256 public totalSupply = 1e18;
-    uint8 public decimals = 18;
+    string  public name = "Tether";
+    string  public symbol = "USDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8   public decimals = 18;
 }
