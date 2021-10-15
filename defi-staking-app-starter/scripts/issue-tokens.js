a9d86be032dfaf1e191d62f1780096a8e9456780
Sachin Singh Jagawat <sachinsinghjagawat@gmail.com>
2021-10-15 11:09:47 +0530

    All errors resolved. Everything working perfectly.(test, scripts, contracts, all working fine)

--- defi-staking-app-starter/scripts/issue-tokens.js
+++ defi-staking-app-starter/scripts/issue-tokens.js
@@ -3,7 +3,12 @@ const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function issueRewards (callback) {
     let decentralBank = await DecentralBank.deployed();
-    await decentralBank.issueTokens();
+    await decentralBank.issueTokens().then(function () {
+        console.log("Promise Resolved");
+   }).catch(function () {
+        console.log("Promise Rejected");
+   });
+
     console.log ("Tokens are successfully issued");
     callback ();
 }
