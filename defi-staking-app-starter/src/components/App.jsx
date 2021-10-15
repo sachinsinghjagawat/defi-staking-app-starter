fe864ec4d4699966cba5208b0775dd6fd01f3213
Sachin Singh Jagawat <sachinsinghjagawat@gmail.com>
2021-10-15 12:18:47 +0530

    Navbar added. No errors

---
+++ defi-staking-app-starter/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+    return <div>
+        <h1>Namaste Block-chain</h1>
+        <Navbar account="0x0" />
+    </div>
+}
+
+export default App;
